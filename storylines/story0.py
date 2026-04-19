bc0b6488b2b47a053df53131638442ef4a536f5e
co16661666 <62578553+co16661666@users.noreply.github.com>
2026-04-18 23:41:09 -0400

    modified index for starting at 0

--- storylines/story0.py
+++ storylines/story0.py
@@ -7,21 +7,21 @@ class Story0:
         self.story_id = 0
         self.story_name = "Basic Cutting"
 
-        # Zones
+        # Zones TODO: Adjust for setup
         self.cuttingZone = Zone("Cutting Zone", 0, 200, 0, 200)
         self.mixingZone = Zone("Mixing Zone", 200, 400, 0, 200)
         self.servingZone = Zone("Serving Zone", 400, 600, 0, 200)
 
-        self.lettuce = Ingredient(6, "Lettuce", initTagPosDict[6], 0, 3)
-        self.tomato = Ingredient(7, "Tomato", initTagPosDict[7], 0, 3)
-        self.mixPlate = MixPlate(5, "Mix Plate", initTagPosDict[5], 0, 1, self.lettuce, self.tomato)
+        self.lettuce = Ingredient(6, "Lettuce", initTagPosDict[5], 0, 3)
+        self.tomato = Ingredient(7, "Tomato", initTagPosDict[6], 0, 3)
+        self.mixPlate = MixPlate(5, "Mix Plate", initTagPosDict[4], 0, 1, self.lettuce, self.tomato)
 
         self.complete = False
 
     def update(self, tagPosDict):
-        self.lettuce.position = tagPosDict[6]
-        self.tomato.position = tagPosDict[7]
-        self.mixPlate.position = tagPosDict[5]
+        self.lettuce.position = tagPosDict[5]
+        self.tomato.position = tagPosDict[6]
+        self.mixPlate.position = tagPosDict[4]
 
     def checkComplete(self):
         if self.servingZone.inZone(self.mixPlate.position) and self.mixPlate.processed:
